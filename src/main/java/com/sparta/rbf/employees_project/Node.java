8f3f31fcfc5becae264fda69f0f85a8a1d2718f9
HusseinAw <46084220+HusseinAw@users.noreply.github.com>
2023-05-10 16:08:34 +0100

    added employee formatter, binary search tree and node skeleton code

---
+++ src/main/java/com/sparta/rbf/employees_project/Node.java
@@ -0,0 +1,19 @@
+package com.sparta.rbf.employees_project;
+
+public class Node {
+    private Employee employee;
+    private Node leftChild;
+    private Node rightChild;
+
+    public Node(Employee employee) {
+        // TO-DO
+    }
+    public boolean isLeftChildEmpty() {
+        // TO-DO
+        return false;
+    }
+    public boolean isRightChildEmpty() {
+        // TO-DO
+        return false;
+    }
+}
