0f73cb5a5dbb01563bf6e0443c215917ae121e4c
Chamara Silva <chamarasilva55@gmail.com>
2023-05-11 16:31:35 +0100

    Made additions to Employee class & added some tests

--- src/main/java/com/sparta/rbf/employees_project/Employee.java
+++ src/main/java/com/sparta/rbf/employees_project/Employee.java
@@ -1,5 +1,7 @@
 package com.sparta.rbf.employees_project;
 
+
+import java.time.LocalDate;
 import java.util.Date;
 
 public class Employee {
@@ -10,8 +12,61 @@ public class Employee {
     private String lastName;
     private Gender gender;
     private String email;
-    private Date dateOfBirth;
-    private Date dateOfJoining;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfJoining;
     private int salary;
 
+    public Employee(int empID, NamePrefix namePrefix, String firstName, char middleInitial, String lastName,
+                    Gender gender, String email, LocalDate dateOfBirth, LocalDate dateOfJoining, int salary) {
+        this.empID = empID;
+        this.namePrefix = namePrefix;
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfJoining = dateOfJoining;
+        this.salary = salary;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public NamePrefix getNamePrefix() {
+        return namePrefix;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public char getMiddleInitial() {
+        return middleInitial;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
 }
---
+++ src/test/java/com/sparta/rbf/employees_project/Employee_Test.java
@@ -0,0 +1,30 @@
+package com.sparta.rbf.employees_project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class Employee_Test {
+    Employee employee1 = new Employee(123456, NamePrefix.Mr, "John", 'P', "Smith",
+            Gender.MALE, "johnsmith@gmail.com", LocalDate.of(1979,10,25),
+            LocalDate.of(2021, 5,10), 44000);
+
+    Employee employee2 = new Employee(987546, NamePrefix.Mrs, "Emma", 'C', "Wood",
+            Gender.FEMALE, "emmacwood@gmail.com", LocalDate.of(1999,8,22),
+            LocalDate.of(2022,3,21), 30000);
+
+    @Test
+    @DisplayName("Check that a new Employee object can be created")
+    void checkForEmployeeInstantiation() {
+        Assertions.assertTrue(employee1 != null);
+    }
+
+    @Test
+    @DisplayName("Check that two different Employee objects will be seen as not equal")
+    void checkEmployeesNotEqual() {
+        Assertions.assertNotEquals(employee1, employee2);
+    }
+
+}
