9805e30440b4c9be79a34c438f8669a39f60638e
Chamara Silva <chamarasilva55@gmail.com>
2023-05-19 11:18:00 +0100

    Made additions to FileWriterFactory

--- src/main/java/com/sparta/rbf/employees_project2/jdbc/jackson/FileWriterFactory.java
+++ src/main/java/com/sparta/rbf/employees_project2/jdbc/jackson/FileWriterFactory.java
@@ -1,6 +1,15 @@
 package com.sparta.rbf.employees_project2.jdbc.jackson;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Logger;
 
 import static com.sparta.rbf.employees_project2.jdbc.App.getFileName;
@@ -29,5 +38,25 @@ public class FileWriterFactory {
             new MappingXML().saveToFile(employees, fileName);
         }
     }
+
+    public static ObjectMapper getMapper(String fileExtension) {
+        if(fileExtension.equals("xml")) {
+            return new XmlMapper();
+        } else {
+            return new ObjectMapper();
+        }
+    }
+
+    public static <T extends ObjectMapper> void saveToFile(T objectMapper, Employees employees, String fileName) {
+        objectMapper.registerModule(new JavaTimeModule()).configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        try {
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("src/main/resources/" + fileName),employees.getEmployees());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
